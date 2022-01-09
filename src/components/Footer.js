4623f0bc7f1507bbb863468b85371f7f0e29dd51
waffe_x20 <argueta_eduardo@yahoo.de>
2022-01-08 20:35:24 -0600

    finish setup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,24 +4,28 @@ import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Link from "@material-ui/core/Link";
-//import Box from "@material-ui/core/Box";
+import Box from "@material-ui/core/Box";
 
 const useStyles = makeStyles((theme) => ({
   footer: {
     borderTop: `1px solid ${theme.palette.divider}`,
     marginTop: theme.spacing(8),
     paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+    [theme.breakpoints.up("sm")]: {
+      paddingTop: theme.spacing(6),
+      paddingBottom: theme.spacing(6),
+    },
   },
 }));
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright "}
+      {"Copyright© "}
       <Link color="inherit" href="https://eduaroargueta.codes">
         Ohms
-      </Link>
-      {""}
+      </Link>{" "}
       {new Date().getFullYear()}
       {"."}
     </Typography>
@@ -46,10 +50,30 @@ function Footer() {
       <Container maxWidth="md" component="footer" className={classes.footer}>
         <Grid container spacing={4} justify="space-evenly">
           {footers.map((footer) => (
-            <Grid item xs={6} sm={3} key={footer.title}></Grid>
+            <Grid item xs={6} sm={3} key={footer.title}>
+              <Typography variant="h6" color="textPrimary" gutterBottom>
+                {footer.title}
+              </Typography>
+              <ul>
+                {footer.description.map((item) => (
+                  <li key={item}>
+                    <Link
+                      rel="stylesheet"
+                      href="#"
+                      variant="subtitle1"
+                      color="textSecondary"
+                    >
+                      {item}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </Grid>
           ))}
         </Grid>
-        <Copyright />
+        <Box mt={5}>
+          <Copyright />
+        </Box>
       </Container>
     </React.Fragment>
   );
