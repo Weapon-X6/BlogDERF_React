094001ae93b3ee5febfe94fa844b59f8a1730e27
waffe_x20 <argueta_eduardo@yahoo.de>
2022-01-13 21:32:23 -0600

    Add initial Posts page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from "react";
-//import "./App.css";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Posts from "./components/Post";
+import PostLoadingComponent from "./components/PostLoading";
 
-class App extends React.Component {
-  componentDidMount() {
-    const apiUrl = "http://127.0.0.1:8000/api/1";
+function App() {
+  const PostLoading = PostLoadingComponent(Posts);
+  const [appState, setAppState] = useState({
+    loading: false,
+    posts: null,
+  });
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = "http://127.0.0.1:8000/api/";
     fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  }
-  render() {
-    return <div>Beispiel</div>;
-  }
+      .then((data) => data.json())
+      .then((posts) => {
+        setAppState({ loading: false, posts: posts });
+      });
+  }, [setAppState]);
+  return (
+    <div className="App">
+      <h1>Latests Posts</h1>
+      <PostLoading isLoading={appState.loading} posts={appState.posts} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/PostLoading.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function PostLoading(Component) {
+  return function PostLoadingComponent({ isLoading, ...props }) {
+    if (!isLoading) return <Component {...props} />;
+    return <p style={{ fontSize: "25px" }}>Waiting for the data to load...</p>;
+  };
+}
+
+export default PostLoading;
